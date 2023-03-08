8fe88269d357448e8acf93f87c72121fa7249f07
Danile Yu <charlesyou1234567@gmail.com>
2023-03-08 10:22:07 +0800

    test

--- src/component/config/moveBack.js
+++ src/component/config/moveBack.js
@@ -20,7 +20,7 @@ const MoveBack = ({ z }) => {
     }
 
     return (
-        <section className="move-back" onDoubleClick={handleClick} style={{ width: "100%", height: "100%", position: "fixed", zIndex: z, left: 0, top: 0, pointerEvents: path == "/" ? "none" : "auto" }}>
+        <section className="move-back" onClick={handleClick} style={{ width: "100%", height: "100%", position: "fixed", zIndex: z, left: 0, top: 0, pointerEvents: path == "/" ? "none" : "auto" }}>
 
         </section>
     )
